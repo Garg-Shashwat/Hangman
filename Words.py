61ad2fe8d2b48bdd3ea5629d4de654c43dd775d1
Shashwat Garg <gargshashwat007@gmail.com>
2020-07-06 18:56:11 +0530

    Update Words.py

--- Words.py
+++ Words.py
@@ -1,5 +1,5 @@
-list=[["apple","banana","orange","avacado","strawberry","pineapple"],["godzilla","robin hood","the fault in our stars","singham","contagion"],["dark","naruto","friends","the big bang theory","breaking bad","game of thrones"],["god of war","horizon","assassin's creed","grand theft auto","call of duty","player unknown battleground"]]
-dict={
+word_list=[["apple","banana","orange","avacado","strawberry","pineapple"],["godzilla","robin hood","the fault in our stars","singham","contagion"],["dark","naruto","friends","the big bang theory","breaking bad","game of thrones"],["god of war","horizon","assassin's creed","grand theft auto","call of duty","player unknown battleground"]]
+word_dict={
 0:"fruits",
 1:"movies",
 2:"series",
