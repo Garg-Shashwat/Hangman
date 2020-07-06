c298ae4a302ff6bc5f64229f602e7f6bf84f3e55
Shashwat Garg <gargshashwat007@gmail.com>
2020-07-06 19:01:30 +0530

    Update main_hm.py

--- main_hm.py
+++ main_hm.py
@@ -3,7 +3,7 @@ import random
 
 def rndm_word():
     word = []
-    num = random.randint(0,2)
+    num = random.randint(0,len(Words.words_dict)-1)
     word.append(random.choice(Words.words_list[num]))
     word.append(Words.words_dict[num])
     return word
