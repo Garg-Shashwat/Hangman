de4881cbb945fc61a168be71191da94b92ff3760
Bhuvnesh-Jindal <67909711+Bhuvnesh-Jindal@users.noreply.github.com>
2020-07-06 18:51:56 +0530

    Add files via upload

---
+++ Words.py
@@ -0,0 +1,7 @@
+list=[["apple","banana","orange","avacado","strawberry","pineapple"],["godzilla","robin hood","the fault in our stars","singham","contagion"],["dark","naruto","friends","the big bang theory","breaking bad","game of thrones"],["god of war","horizon","assassin's creed","grand theft auto","call of duty","player unknown battleground"]]
+dict={
+0:"fruits",
+1:"movies",
+2:"series",
+3:"games"
+}
