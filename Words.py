6c1fe5aa1b5d5386f628044c104a9b24d7b1381a
Shashwat Garg <gargshashwat007@gmail.com>
2020-07-06 18:57:39 +0530

    First test

--- Words.py
+++ Words.py
@@ -1,5 +1,5 @@
-word_list=[["apple","banana","orange","avacado","strawberry","pineapple"],["godzilla","robin hood","the fault in our stars","singham","contagion"],["dark","naruto","friends","the big bang theory","breaking bad","game of thrones"],["god of war","horizon","assassin's creed","grand theft auto","call of duty","player unknown battleground"]]
-word_dict={
+words_list=[["apple","banana","orange","avacado","strawberry","pineapple"],["godzilla","robin hood","the fault in our stars","singham","contagion"],["dark","naruto","friends","the big bang theory","breaking bad","game of thrones"],["god of war","horizon","assassin's creed","grand theft auto","call of duty","player unknown battleground"]]
+words_dict={
 0:"fruits",
 1:"movies",
 2:"series",
--- main_hm.py
+++ main_hm.py
@@ -1,11 +1,11 @@
-#import words
+import Words
 import random
 
 def rndm_word():
     word = []
     num = random.randint(0,2)
-    word.append(random.choice(words_list[num]))
-    word.append(dictio[num])
+    word.append(random.choice(Words.words_list[num]))
+    word.append(Words.words_dict[num])
     return word
     
 i = rndm_word()
