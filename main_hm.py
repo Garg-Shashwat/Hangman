b798cad9e27be25b3f01d864687b0bd0b5d9a679
Shashwat Garg <gargshashwat007@gmail.com>
2020-07-07 17:10:55 +0530

    Word checking added

--- main_hm.py
+++ main_hm.py
@@ -8,5 +8,27 @@ def rndm_word():
     word.append(Words.words_dict[num])
     return word
     
-i = rndm_word()
-print(i)
+result = rndm_word()
+word, topic = result[0], result[1]
+print(word, topic)
+tries = 6
+ans = '*' * len(word)
+while tries > 0:
+    print("____________________________________________________")
+    print("Topic: " + topic)
+    print("Lives:", tries)
+    print("____________________________________________________")
+    print("Word: "+ ans)
+    a = input("Enter a character: ").lower()
+    if len(a)==1 and a.isalpha():
+        if a in word:
+            to_list = list(ans)
+            for i in range(len(word)):
+                if a == word[i]:
+                    to_list[i] = a
+            ans = "".join(to_list)
+        else:
+            tries-=1
+    else:
+        print("Invalid input!")
+        continue
