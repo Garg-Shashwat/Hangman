1c5518ed39798c0cf373950b8c55209807909dda
Shashwat Garg <gargshashwat007@gmail.com>
2020-07-20 19:35:43 +0530

    Replay added
    difficulty input changed to integers

--- main_hm.py
+++ main_hm.py
@@ -2,77 +2,88 @@ import Words
 import random
 import re
 from os import system
-
+import sys
 
 def difficulty():
+    _ = system('cls')
     while True:
-            difficult = str(input("Please Select Difficulty: Easy, Medium or Hard? ").strip().lower())
-            if difficult == "easy":
+        try:
+            difficult = input("Please Select Difficulty:\n1.Easy\n2.Medium\n3.Hard\nE.Exit\n(1/2/3/E):").strip().lower()
+            _ = system('cls')
+            difficult = int(difficult)
+            if difficult == 1:
                 tries=12
                 break
-            elif difficult == "medium":
+            elif difficult == 2:
                 tries=8
                 break
-            elif difficult == "hard":
+            elif difficult == 3:
                 tries=4
                 break
             else:
                 print("Enter a Valid Difficulty")
-    return tries
+        except:
+            if difficult == 'e':
+                sys.exit("Program terminated: User Interruption")
+            print("Invalid input")
+    return tries, difficult
 
-def rndm_word():
-    words = []
+def rndm_word(difficult):
     num = random.randint(0,len(Words.words_dict)-1)
-    if tries == 12:
-        words.append(random.choice(Words.easy[num]))
-    if tries == 8:
-        words.append(random.choice(Words.medium[num]))
-    if tries == 4:
-        words.append(random.choice(Words.hard[num]))
-    words.append(Words.words_dict[num])
-    return words
+    if difficult == 1:
+        words = random.choice(Words.easy[num])
+    elif difficult == 2:
+        words = random.choice(Words.medium[num])
+    elif difficult == 3:
+        words = random.choice(Words.hard[num])
+    index = Words.words_dict[num]
+    return words, index
 
-tries = difficulty()
-result = rndm_word()
-word, topic = result[0], result[1]
-guessed_letters = []
-solved = False
-test = re.sub('[a-zA-Z]','*', word)
-_ = system('cls')
-while tries > 0:
-    print("____________________________________________________")
-    print("Topic: " + topic)
-    print("Lives:", tries)
-    print("____________________________________________________")
-    print("Word: "+ test)
-    a = input("Enter a character: ").upper()
+replay = 'y'
+while replay == 'y':
+    tries, difficult = difficulty()
+    word, topic = rndm_word(difficult)
+    guessed_letters = []
+    solved = False
+    test = re.sub('[a-zA-Z]','*', word)
     _ = system('cls')
-    if len(a)==1 and a.isalpha():
-        if a in guessed_letters:
-            print("You have already guessed this word")
-            continue
-        elif a in word:
-            guessed_letters.append(a)
-            to_list = list(test)
-            for i in range(len(word)):
-                if a == word[i]:
-                    to_list[i] = a
-            test = "".join(to_list)
-        else:
-            tries-=1
-            print('Letter not found')
-        if '*' not in test:
+    while tries > 0:
+        print("____________________________________________________")
+        print("Topic: " + topic)
+        print("Lives:", tries)
+        print("____________________________________________________")
+        print("Word: "+ test)
+        a = input("Enter a character: ").upper()
+        _ = system('cls')
+        if len(a)==1 and a.isalpha():
+            if a in guessed_letters:
+                print("You have already guessed this word")
+                continue
+            elif a in word:
+                guessed_letters.append(a)
+                to_list = list(test)
+                for i in range(len(word)):
+                    if a == word[i]:
+                        to_list[i] = a
+                test = "".join(to_list)
+            else:
+                tries-=1
+                print('Letter not found')
+            if '*' not in test:
+                solved = True
+                break
+        elif len(a)==len(word) and a==word:
             solved = True
             break
-    elif len(a)==len(word) and a==word:
-        solved = True
-        break
+        else:
+            print("Invalid input!")
+            continue
+    _ = system('cls')
+    print('The answer is:',word)
+    if solved:
+        print("Congrats you won!!!\n:D")
     else:
-        print("Invalid input!")
-        continue
-_ = system('cls')
-print('The answer is:',word)
-if solved:
-    print("Congrats you won!!!\n:D")
-else:
-    print("Sorry you are hanged\n:(")
+        print("Sorry you are hanged\n:(")
+    replay = input("Want to play again?(Y/N)").strip().lower()
+    if replay != '[yYnN]':
+        print("Invalid Input")
